2f62e16d7e68829f9f49f54239c225ea3ce393c6
c.ning0 <c.ning0@f8d984ea-ae41-42e2-7831-692f7f059c9f>
2010-10-08 15:49:42 +0000

    Added one more way to get IR. Repacked as thin Jar.

--- Prevision/src/hk/ust/cse/Prevision/Wala/Jar2IR.java
+++ Prevision/src/hk/ust/cse/Prevision/Wala/Jar2IR.java
@@ -1,6 +1,7 @@
 package hk.ust.cse.Prevision.Wala;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 import com.ibm.wala.classLoader.IMethod;
 import com.ibm.wala.ipa.callgraph.impl.Everywhere;
@@ -19,34 +20,26 @@ public class Jar2IR {
     // Create a name representing the method whose IR we will visualize
     MethodReference mr = StringStuff.makeMethodReference(methodSig);
 
-    IR ir = null;
-    if (mr != null) {
-      // Resolve the method name into the IMethod, the canonical representation of the method information.
-      IMethod m = walaAnalyzer.getClassHierarchy().resolveMethod(mr);
-      if (m == null) {
-        // Throws an exception here
-        Assertions.UNREACHABLE("could not resolve " + mr);
-      }
-      
-      // Build the IR and cache it.
-      ir = walaAnalyzer.getAnalysisCache().getSSACache().findOrCreateIR(m, 
-          Everywhere.EVERYWHERE, walaAnalyzer.getAnalysisOptions().getSSAOptions());
-
-      if (ir == null) {
-        // Throws an exception here
-        Assertions.UNREACHABLE("Null IR for " + m);
-      }
-
-      //System.err.println(ir.toString());
-    }
-    return ir;
+    return getIRFromMethodReference(walaAnalyzer, mr);
   }
   
-  public static IR getIR(WalaAnalyzer walaAnalyzer, String methodName, int nLine) {
+  public static IR getIR(WalaAnalyzer walaAnalyzer, Method method) {
     // Create a name representing the method whose IR we will visualize
     MethodReference mr = Jar2IRUtils.getMethodReference(
-        walaAnalyzer.getClassHierarchy(), methodName, nLine);
+        walaAnalyzer.getClassHierarchy(), method);
     
+    return getIRFromMethodReference(walaAnalyzer, mr);
+  }
+  
+  public static IR getIR(WalaAnalyzer walaAnalyzer, String methodName, int lineNo) {
+    // Create a name representing the method whose IR we will visualize
+    MethodReference mr = Jar2IRUtils.getMethodReference(
+        walaAnalyzer.getClassHierarchy(), methodName, lineNo);
+    
+    return getIRFromMethodReference(walaAnalyzer, mr);
+  }
+  
+  private static IR getIRFromMethodReference(WalaAnalyzer walaAnalyzer, MethodReference mr) {
     IR ir = null;
     if (mr != null) {
       // Resolve the method name into the IMethod, the canonical representation of the method information.
